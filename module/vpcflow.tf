be43b10e03e9deb5aa933dbc719becfc7ddb5839
princegalland <111612241+princegalland@users.noreply.github.com>
2024-08-31 11:34:26 -0400

    Update vpcflow.tf

--- module/vpcflow.tf
+++ module/vpcflow.tf
@@ -1,13 +1,13 @@
 # Configuring A VPC Flow
 
- resource "aws_flow_log" "vpc1flow" {
-    vpc_id = aws_vpc.vpc1.id
-    traffic_type = "ALL"
-    log_destination_type = "s3"
+# resource "aws_flow_log" "vpc1flow" {
+ #   vpc_id = aws_vpc.vpc1.id
+ #   traffic_type = "ALL"
+  #  log_destination_type = "s3"
 
-    log_destination = aws_s3_bucket.clinton12.arn
+ #   log_destination = aws_s3_bucket.clinton12.arn
     
-  tags = {
-    Name = "vpc1flow"
-  }
- }
+ # tags = {
+  #  Name = "vpc1flow"
+ # }
+# }
